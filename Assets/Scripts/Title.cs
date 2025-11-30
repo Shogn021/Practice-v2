6610df303361cc37d425d9ba3b8205803231bfa7
Shogn021 <shogn021@gmail.com>
2025-11-30 18:58:40 +0900

    플레이어 생성
    [추가] 플레이어 에셋
    [추가] 플레이어 Idle, 걷기, 달리기 구현
    [추가] Player Manager 스크립트 추가
    [추가] Character Controller 스크립트 추가
    [삭제] Player Controller 스크립트

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -3,7 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    #region Singleton
+    public static Title instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
     public string sceneName;
+    private PlayerManager thePlayer;
+
+    private void Start()
+    {
+        thePlayer = FindAnyObjectByType<PlayerManager>();
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(sceneName);
@@ -12,5 +34,11 @@ public class Title : MonoBehaviour
     public void GoToTitle()
     {
         SceneManager.LoadScene("Title");
+    }
+
+    public void Resume()
+    {
+        thePlayer.escTab.SetActive(false);
+        thePlayer.canMove = true;
     }
 }
